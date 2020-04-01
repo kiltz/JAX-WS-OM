510d51cd448beba972564ff4df5843aff926ef32
Friedrich <f@kiltz.de>
2020-04-01 11:09:38 +0200

    incl. generierter Dateien

---
+++ ws-Provider/src/generated/java/de/kiltz/kontakt/ws/v1/Suche.java
@@ -0,0 +1,60 @@
+
+package de.kiltz.kontakt.ws.v1;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java-Klasse für suche complex type.
+ * 
+ * <p>Das folgende Schemafragment gibt den erwarteten Content an, der in dieser Klasse enthalten ist.
+ * 
+ * <pre>
+ * &lt;complexType name="suche">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="such" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "suche", propOrder = {
+    "such"
+})
+public class Suche {
+
+    protected String such;
+
+    /**
+     * Ruft den Wert der such-Eigenschaft ab.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getSuch() {
+        return such;
+    }
+
+    /**
+     * Legt den Wert der such-Eigenschaft fest.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setSuch(String value) {
+        this.such = value;
+    }
+
+}
---
+++ ws-Provider/src/generated/java/de/kiltz/kontakt/ws/v1/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://kiltz.de/kontakt/ws/v1/")
+package de.kiltz.kontakt.ws.v1;
